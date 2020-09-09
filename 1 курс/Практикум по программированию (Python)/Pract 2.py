39b040a8e4f7f771c8c681f825c5125ac324d13d
Cat-in-box <69214647+Cat-in-box@users.noreply.github.com>
2020-09-09 18:27:20 +0300

    Great

---
+++ 1 курс/Практикум по программированию (Python)/Pract 2.py
@@ -0,0 +1,140 @@
+#ТА САМАЯ ПРОГРАММА С ИНТЕРФЕЙСОМ
+# Калькулятор дробей
+
+Results = {}
+Result = 0
+
+def PUSK():
+    global Result, Results
+    inp = input('Ввод ')
+    
+    if 'save' in inp:
+        Results[inp[inp.index(' ')+1:]]=Result
+        print('save success')
+        PUSK()
+    elif 'del' in inp:
+        Results.pop(inp[inp.index(' ')+1:])
+        print('delete success')
+        PUSK()
+    elif inp in Results.keys():
+        Result=Results[inp]
+        if Result[0]<Result[2]:
+            print(str(Result[0]) + '/' + str(Result[2]))
+        elif Result[0]>Result[2]:
+            print(str(Result[0]//Result[2]) + '(' + str(Result[0]%Result[2]) + '/' + str(Result[2]) + ')')
+        else:
+            print(1)
+        PUSK()
+    elif inp=='to double':
+        print(Result[0]/Result[2])
+        PUSK()
+    elif inp=='exit':
+        None
+    else:
+        
+        inp = inp.replace(' ', '')
+        
+        for key in Results.keys():
+            inp = inp.replace(key, str(Results[key][0]) + '/' + str(Results[key][2]))
+        
+        inp = list(inp)
+        
+        i = 0
+        while i<(len(inp)): #Соединение цифр в многозначных числах
+            try:
+                inp[i] = int(inp[i])
+                inp[i+1] = int(inp[i+1])
+            except:
+                i+=1
+            else:
+                inp[i]=int(str(inp[i])+str(inp[i+1]))
+                inp.pop(i+1)
+
+        for i in range(len(inp)-1): #Замена вычитания на сложение с отрицательным числом
+            if (inp[i]=='-') and (i==0):
+                inp.pop(i)
+                inp[i] = -inp[i]
+            elif inp[i]=='-':
+                inp[i] = '+'
+                inp[i+1] = -inp[i+1]
+        
+        for i in range(len(inp)-5): #Перевод смешанных чисел в неправильные дроби
+            if (type(inp[i])==int) and (inp[i+1]=='(') and (type(inp[i+2])==int) and (inp[i+3]=='/') and (type(inp[i+4])==int) and (inp[i+5]==')'):
+                inp[i+2]+=inp[i]*inp[i+4]
+                inp.pop(i)
+                inp.pop(i)
+                inp.pop(i+3)
+
+        def brackets(X): #Работа со скобками
+            X.remove('(')
+            counter=0
+            start=0
+            while counter<X.count(')'):
+                start = X.index(')', start,)
+                counter+=1
+            X.pop(start)
+            return X
+
+        def Add(A, B): #Сумма
+            if (len(A)==1) and (len(B)==1):
+                return [A[0]+B[0]]
+            elif (len(A)==1) and (len(B)==3):
+                return [A[0]*B[2]+B[0], '/', B[2]]
+            elif (len(A)==3) and (len(B)==1):
+                return [B[0]*A[2]+A[0], '/', A[2]]
+            elif (len(A)==3) and (len(B)==3):
+                return [A[0]*B[2]+B[0]*A[2], '/', A[2]*B[2]]
+
+        def Mult(A, B): #Произведение
+            if (len(A)==1) and (len(B)==1):
+                return [A[0]*B[0]]
+            elif (len(A)==1) and (len(B)==3):
+                return [A[0]*B[0], '/', B[2]]
+            elif (len(A)==3) and (len(B)==1):
+                return [B[0]*A[0], '/', A[2]]
+            elif (len(A)==3) and (len(B)==3):
+                return [A[0]*B[0], '/', A[2]*B[2]]
+
+        primitives = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499)
+
+        def DivRecursion(A, B): #Поиск делителей
+            for nod in primitives:
+                if (A[0]%nod==0) and (B[0]%nod==0):
+                    A[0]=A[0]//nod
+                    B[0]=B[0]//nod
+                    DivRecursion(A, B)
+
+        def Div(A, B): #Деление
+            DivRecursion(A, B)
+            return [A[0], '/', B[0]]
+
+        def F(X):
+            if ('+' in X) and ((('(' not in X) and (')' not in X)) or (X.index('(')>X.index('+')) or (X.index(')')<X.index('+'))):
+                A = X[0:X.index('+')]
+                B = X[X.index('+')+1:]
+                return (Add(F(A), F(B)))
+            elif ('*' in X) and ((('(' not in X) and (')' not in X)) or (X.index('(')>X.index('*')) or (X.index(')')<X.index('*'))):
+                A = X[0:X.index('*')]
+                B = X[X.index('*')+1:]
+                return (Mult(F(A), F(B)))
+            elif ('/' in X) and ((('(' not in X) and (')' not in X)) or (X.index('(')>X.index('/')) or (X.index(')')<X.index('/'))):
+                A = X[0:X.index('/')]
+                B = X[X.index('/')+1:]
+                return (Div(F(A), F(B)))
+            elif ('(' in X) and (')' in X):
+                brackets(X)
+                return(F(X))
+            else:
+                return X
+        
+        Result = F(inp)
+        Result = Div([Result[0]], [Result[2]])
+        
+        if Result[0]<Result[2]:
+            print(str(Result[0]) + '/' + str(Result[2]))
+        elif Result[0]>Result[2]:
+            print(str(Result[0]//Result[2]) + '(' + str(Result[0]%Result[2]) + '/' + str(Result[2]) + ')')
+        else:
+            print(1)
+        PUSK()
+PUSK()
