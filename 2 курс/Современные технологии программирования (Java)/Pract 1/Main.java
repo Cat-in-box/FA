25f9c9b7a1cc7c836a603b23e9990575ca70289b
Cat-in-box <69214647+Cat-in-box@users.noreply.github.com>
2020-09-21 22:02:20 +0300

    Reorganization

---
+++ 2 курс/Современные технологии программирования (Java)/Pract 1/Main.java
@@ -0,0 +1,12 @@
+package my.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world!");
+        Matrix matrix1 = new Matrix(5,5);
+        matrix1.Ones();
+        matrix1.PrintMatrix();
+    }
+}
---
+++ 2 курс/Современные технологии программирования (Java)/Pract 1/Matrix.java
@@ -0,0 +1,31 @@
+package my.company;
+
+public class Matrix {
+    private int matr[][];
+
+    public Matrix(int m, int n) {
+        this.matr = new int[m][n];
+    }
+
+    public void Ones() {
+        for (int i = 0; i < matr.length; i++) {
+            for (int j = 0; i < matr[i].length; j++) {
+                if (i == j) {
+                    matr[i][j] = 1;
+                }
+                else {
+                    matr[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void PrintMatrix() {
+        for (int i = 0; i < matr.length; i++) {
+            for (int j = 0; i < matr[i].length; j++){
+                System.out.print(matr[j][i]);
+            }
+        System.out.println("");
+        }
+    }
+}
