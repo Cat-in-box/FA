ad56d71fa9586624231553e34d7002fa408f5942
KOSHKA <69214647+Cat-in-box@users.noreply.github.com>
2021-02-18 09:58:20 +0300

    Update Main.java

--- 2 курс/Современные технологии программирования (Java)/Hw_1/src/Task2/Main.java
+++ 2 курс/Современные технологии программирования (Java)/Hw_1/src/Task2/Main.java
@@ -1,8 +1,14 @@
 package Task2;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        FileProcessor fileProcessor = new FileProcessor();
+        fileProcessor.FileWriter("Example.txt");
+
 
     }
 }
+
