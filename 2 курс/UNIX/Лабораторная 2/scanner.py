669b72c6d5841500788ed6fa84de67a24ab2fab4
KOSHKA <69214647+Cat-in-box@users.noreply.github.com>
2021-04-14 09:33:15 +0300

    Добавили прогресс
    Он закомменчен

--- 2 курс/UNIX/Лабораторная 2/scanner.py
+++ 2 курс/UNIX/Лабораторная 2/scanner.py
@@ -1,33 +1,47 @@
 import socket
 import threading
 from threading import Thread
+import time
  
 class T(threading.Thread):
-	def __init__(self, Number_all, n, host):
-		self.Number_all = Number_all
-		self.n = n
-		self.host = host
-		threading.Thread.__init__(self, name="t" + str(self.n))
+    def __init__(self, Number_all, n, host):
+        self.Number_all = Number_all
+        self.n = n
+        self.host = host
+        self.progress = 0
+        threading.Thread.__init__(self, name="t" + str(self.n))
 	
-	def run(self):
-		print("Процесс", self.n)
-		K = self.n*10000
-		if K > self.Number_all:
-			K = (self.n-1)*10000 - self.Number_all
+    def run(self):
+        print("Процесс", self.n)
+        K = self.n*10000
+        if K > self.Number_all:
+            K = (self.n-1)*10000 - self.Number_all
 
-		for port in range((self.n-1)*10000 + 1, K):
-			sock = socket.socket()
-			try:
-				print(port)
-				sock.connect((self.host, port))
-				print("Порт", port, "открыт")
-			except:
-				continue
+        for port in range((self.n-1)*10000 + 1, K):
+            sock = socket.socket()
+            self.progress = port - (self.n-1)*10000 + 1
+            try:
+                print(port)
+                sock.connect((self.host, port))
+                print("Порт", port, "открыт")
+            except:
+                continue
 
 def scann():
-	Number_all = 2**16 - 1
+    Number_all = 2**16 - 1
+    #p_list = []
+    host = input('Введите имя хоста/IP-адрес')
+    for p_number in range(Number_all // 10000 + 1):
+        p = T(Number_all, p_number+1, host)
+        #p_list.append(p)
+        p.start()
     
-	host = input('Введите имя хоста/IP-адрес')
-	for p_number in range(Number_all // 10000 + 1):
-		p = T(Number_all, p_number+1, host)
-		p.start()
+    '''
+    flag = True
+    while flag:
+        count = round(100*sum(p.progress for p in p_list)/Number_all, 2)
+        if count == 100.0:
+            flag = False
+        print("Прогресс: " + str(count) + '%')
+        time.sleep(5)
+    '''
