d7b622ec48de86f2a223ee41b7bf5d5cf41ebaa0
KOSHKA <69214647+Cat-in-box@users.noreply.github.com>
2021-04-14 09:33:49 +0300

    Update client.py

--- 2 курс/UNIX/Лабораторная 2/client.py
+++ 2 курс/UNIX/Лабораторная 2/client.py
@@ -6,23 +6,6 @@ sock = socket.socket()
 sock.setblocking(1)
 scanner.scann()
 
-'''
-N = 2**16 - 1
-
-host = input('Введите имя хоста/IP-адрес')
-
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect((host, port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
-'''
-
 #msg = input()
 msg = "Hi!"
 sock.send(msg.encode())
