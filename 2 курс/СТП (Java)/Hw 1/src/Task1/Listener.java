fa66a6954884839ad614cdd2dc6587f5a0a88578
KOSHKA <69214647+Cat-in-box@users.noreply.github.com>
2021-12-06 19:00:26 +0300

    Delete 2 курс directory

--- 2 курс/СТП (Java)/Hw 1/src/Task1/Listener.java
+++
@@ -1,8 +0,0 @@
-package Task1;
-
-class Listener implements StringBuilderListener {
-
-    public void Changed(StringBuilderWr StringBuilder) {
-        System.out.println("Строка обновлена: " + StringBuilder.toString());
-    }
-}
